50d5b6bdbf869fc744914f9ce617940504147ea9
fuwamaki <variety.fuwa.3@gmail.com>
2021-12-14 12:35:53 +0900

    fix

--- node/slack-emoji-export.js
+++ node/slack-emoji-export.js
@@ -2,7 +2,7 @@ var Slack = require('slack-node');
 var request = require('request');
 var fs = require('fs');
 
-apiToken = "xoxp-308073870612-307990994083-654907275393-e16777ff27a712b125503439716821ae"; // ここにAPIトークンを貼り付ける。
+apiToken = "xxx"; // ここにAPIトークンを貼り付ける。
 slack = new Slack(apiToken);
 
 slack.api("emoji.list", function (err, response) {
