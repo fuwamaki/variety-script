38fb0fecadfc784978ea45dcf53cf21220681c86
fuwamaki <variety.fuwa.3@gmail.com>
2022-02-11 09:49:27 +0900

    create SubstringPlayground

---
+++ swift/SubstringPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var text1 = "ビビデバビデブー"
+//if let range = text1.range(of: "ビデ") {
+//    text1.removeSubrange(range)
+//    print(text1)
+//}
+
+//if let range = text1.range(of: "ビデ") {
+//    text1.replaceSubrange(range, with: "")
+//    print(text1)
+//}
+
+//text1.removeAll()
+//print(text1)
+
+var text2 = ""
+
+
+// starts
